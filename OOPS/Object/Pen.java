076e2ba0efdd6a840116e2b73fcbbb1ddf51a2de
Tasneem Aamir <117052484+tasneemaamir20@users.noreply.github.com>
2024-05-17 11:45:26 +0530

    Add files via upload

---
+++ OOPS/Object/Pen.java
@@ -0,0 +1,16 @@
+public class Pen
+{
+    String color;
+    String brand;
+    int price;
+
+    Pen(String color,String brand,int price)
+    {
+        this.
+    }
+
+    public static void main(String[] args) {
+        Pen p=new Pen();
+        System.out.println(p);
+    }
+}
